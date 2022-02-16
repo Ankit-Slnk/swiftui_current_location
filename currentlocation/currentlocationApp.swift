b1fff833ba2a3c4c99412fd6656578bbe9c1e692
Ankit Solanki <ankit_ms95@yahoo.com>
2022-02-16 23:22:18 +0530

    Initial Commit

---
+++ currentlocation/currentlocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  currentlocationApp.swift
+//  currentlocation
+//
+//  Created by Ankit Solanki on 16/02/22.
+//
+
+import SwiftUI
+
+@main
+struct currentlocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
