97e4617fa84f620b674c17fcdf9a5abe8c7824bd
Ankit Solanki <ankit_ms95@yahoo.com>
2022-02-16 23:31:27 +0530

    initial commit

--- currentlocation/ContentView.swift
+++ currentlocation/ContentView.swift
@@ -6,10 +6,30 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    
+    var userLatitude: String {
+        let l = "\(locationManager.lastLocation?.coordinate.latitude ?? 0.0)"
+        print("userLatitude")
+        print(l)
+        return l
+    }
+    
+    var userLongitude: String {
+        let l = "\(locationManager.lastLocation?.coordinate.longitude ?? 0.0)"
+        print("userLongitude")
+        print(l)
+        return l
+    }
+    
     var body: some View {
-        Text("Hello, world!")
+        print(userLatitude) // dont remove this
+        print(userLongitude)  // dont remove this
+        
+        return Text((userLatitude == "0.0" || userLongitude == "0.0") ? "Hello, world!" : (userLatitude + ", " + userLongitude))
             .padding()
     }
 }
